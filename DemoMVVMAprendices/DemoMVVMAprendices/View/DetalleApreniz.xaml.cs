f5ed680b167893f5fb2023ad833521d508b0ab7c
lorenaquint <lorenaquint@gmail.com>
2018-04-19 23:52:47 -0500

    Initial commit

---
+++ DemoMVVMAprendices/DemoMVVMAprendices/View/DetalleApreniz.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+namespace DemoMVVMAprendices.View
+{
+    using DemoMVVMAprendices.Model;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetalleApreniz : ContentPage
+	{
+		public DetalleApreniz (Aprendiz aprendizSeleccionado)
+		{
+			InitializeComponent ();
+            this.BindingContext = aprendizSeleccionado;
+		}
+	}
+}
